4d8227b2c7917cad5905d47fb317ab4a134499c5
Robel Nani <45096788+RobelTadele@users.noreply.github.com>
2021-04-03 19:18:22 -0500

    Add files via upload

---
+++ Matching Game2/Matching Game2/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matching_Game2
+{
+    public partial class Form1 : Form
+    {
+
+
+        //Generates random numbers
+        Random random = new Random();
+
+
+        //List of Lettered Icons
+        List<String> icons = new List<String>() { "Q", "M", "T", "V", "B", "J", "W", "Z", "Q", "M", "T", "V", "B", "J", "W", "Z" };
+
+
+        //Keeps track of Labels Clicked
+        Label firstClicked = null;
+        Label secondClicked = null;
+
+
+        //Method Assigns icons from list to random Grid
+
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+
+                    //Hiding Icons from user
+                    iconLabel.ForeColor = iconLabel.BackColor;
+
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void fILEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label_click(object sender, EventArgs e)
+        {
+
+            if (timer1.Enabled == true)
+                return;
+
+
+            Label clickedLabel = sender as Label;
+
+            //Runs if Label Control has successfully been converted into a Label Control
+            if (clickedLabel != null)
+            {
+
+                //If Icon is visible exit method if not change icon to black 
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+
+
+
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+
+                //Check to see if the player won
+                checkForWinner();
+
+
+                //If two icons match reset reference Variables
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
+
+                timer1.Start();
+
+            }
+        }
+
+        
+
+
+        private void checkForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+
+                if (iconLabel != null)
+                {
+                    //If icon isn't visible : player hasn't found mathcing icons
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+
+
+
+
+            MessageBox.Show("Hooray! You've matched all the icons!", "Congrats!");
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            // Stop the timer
+            timer1.Stop();
+
+            // Hide both icons
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Displays About Page Dialogue
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+            AboutForm dial = new AboutForm();
+            dial.ShowDialog();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Exits program when Exit button is clicked
+            this.Close();
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HelpDialog helper = new HelpDialog();
+            helper.ShowDialog();
+        }
+    }
+}
---
+++ Matching Game2/Matching Game2/Form2.Designer.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Matching_Game2
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+            this.SuspendLayout();
+            // 
+            // AboutForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.BackgroundImage = global::Matching_Game2.Properties.Resources._3840x1080_Wallpaper_146_1_;
+            this.ForeColor = System.Drawing.Color.Crimson;
+            this.HelpButton = true;
+            this.Name = "AboutForm";
+            this.Load += new System.EventHandler(this.AboutForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
